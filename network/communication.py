0634238203312d412c4cc68f9869e98ecdf348be
Diogo Ribeiro <ribeiropdiogo@gmail.com>
2021-05-20 23:48:34 +0100

    add comments

--- network/communication.py
+++ network/communication.py
@@ -2,6 +2,12 @@ from interface import interface
 import json
 
 async def get_timeline(server,username,target):
+    value = await server.get(target)
+    userInfo = json.loads(value)
+    # Check if the user has updated content
+    # If not, check the messages that are missing
+    # Make TCP Request directly to target and ask for timeline
+    # Save to local timeline
     return 0
 
 async def follow_user(server,followingUser,followingUserPort,followedUser):
@@ -20,4 +26,8 @@ async def follow_user(server,followingUser,followingUserPort,followedUser):
     interface.printMenu()
 
 async def publish(server,username,content):
+    # Save to local timeline
+    # Get list of subscribers
+    # Send content over tcp to some subscribers (50%)
+    # (the users update vector clock on receive)
     return 0;
